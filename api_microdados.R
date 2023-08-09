90845f14d868aa4c428c299f0d6ec1f35866f200
Thalis Rebouças de Oliveira <50429343+thalisreboucas@users.noreply.github.com>
2023-08-09 15:05:26 -0300

    Add files via upload

--- api_microdados.R
+++ api_microdados.R
@@ -21,5 +21,18 @@ microdados_tbl <- tbl(conexao,"microdados_vinculos") %>%
     ano >= 2013 ,sigla_uf == "CE"
   )
 
-
+# Base de dados
 tbl_rais_ce <- microdados_tbl |> collect()
+tbl_rais_ce <- read.csv("rais_ce.csv")
+#Nomes dos bairros
+nomes_rais_ce <- read.csv2("Bairros_fortaleza_rais.csv")
+# Colocando o nome dos bairros juntos
+tbl_rais_ce <- left_join(tbl_2021,nomes_rais_ce,by = "bairros_fortaleza")
+
+# 
+
+tbl_2021 <- tbl_rais_ce |> 
+  filter(ano == 2021) |> 
+  select(ano,sigla_uf,bairros_fortaleza)
+
+left_join(tbl_2021,nomes_rais_ce,by = "bairros_fortaleza")
